0daf711a03ff5e4b680e2a9cab46a767d06f9024
Sophie Pham <trangphamthu.92@gmail.com>
2020-12-28 15:32:23 +0700

    Give different empty state between favorite & skip list

--- src/components/layout.js
+++ src/components/layout.js
@@ -21,21 +21,19 @@ export const SideBar = ({viewSelected, selectView, viewedProfiles}) => {
 }
 
 const SideBarMain = ({viewSelected, viewedProfiles}) => {
-    return <Row gutter={[16, 16]} style={{overflow: 'scroll', padding: 8,margin: 0, width: '100%', maxHeight: 'calc(100vh - 112px)'}}>
-        {
-            viewedProfiles.length 
+    const profiles = viewedProfiles.filter(({liked}) => viewSelected === 'favorites' ? liked: !liked);
+    return  <Row gutter={[16, 16]} style={{overflow: 'scroll', padding: 8,margin: 0, width: '100%', maxHeight: 'calc(100vh - 112px)'}}>
+        {   
+            profiles.length  
             ?
-            viewedProfiles.map(({liked, imgUrl, lastName}) => {
-                const isShow = viewSelected === 'favorites' ? liked: !liked
-                return isShow ?
-                    <Col span={8} style={{height: 120}}>
-                        <img style={{ objectFit: 'cover', borderRadius: 4, width: '100%', height: '100%'}} src={imgUrl} />
-                        <div style={{position: 'relative', borderRadius: '0px 0px 4px 4px', bottom: 22, paddingLeft: 8, fontWeight: '600', background: 'rgba(0,0,0,0.5)', color: 'white'}}>{lastName}</div>
-                    </Col> 
-                    : null           
-            })
-            :
-            <Empty style={{width: '100%', padding: '72px 0'}}/>
+            profiles.map(({imgUrl, lastName}) => {
+                return  <Col span={8} style={{height: 120}}>
+                    <img style={{ objectFit: 'cover', borderRadius: 4, width: '100%', height: '100%'}} src={imgUrl} />
+                    <div style={{position: 'relative', borderRadius: '0px 0px 4px 4px', bottom: 22, paddingLeft: 8, fontWeight: '600', background: 'rgba(0,0,0,0.5)', color: 'white'}}>{lastName}</div>
+                </Col>         
+            }) : <Empty 
+                description={viewSelected === 'favorites' ? 'No favorite': 'No skip'} 
+                style={{width: '100%', padding: '72px 0'}} />
         }
-    </Row> 
+    </Row>        
 }
